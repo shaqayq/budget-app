5fc9767026a908e1480f8870a350277273c824f7
shaqayq <shaqayq.darwazi@gmail.com>
2022-11-21 15:19:42 +0430

    create database table

--- db/migrate/20221121103537_create_transaction_categories.rb
+++ db/migrate/20221121103537_create_transaction_categories.rb
@@ -3,6 +3,7 @@ class CreateTransactionCategories < ActiveRecord::Migration[7.0]
     create_table :transaction_categories do |t|
       t.references :transaction , null: false , foreign_key: true
       t.references :category , null: false , foreign_key: true
+  
       t.timestamps
     end
   end
